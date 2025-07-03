e0a0b5b724b32778859eeab3627d5e68c9b6d831
D410W <thalesjdc61@gmail.com>
2025-07-03 10:24:26 -0300

    maiores alteraçoes na classe Paleta. criaçao de inicializadores, funçoes de consulta e debug.
    classes.hpp devera ser dividida em classes especificas, ao inves de apenas 'classes'.
    
    main.cpp sendo usada apenas para debug.

--- classes.hpp
+++ classes.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+
 namespace stc {
 
   struct Cor {
@@ -7,14 +10,62 @@ namespace stc {
   };
 
   class Paleta {
+  private:
     int quantidade;
     Cor cores[100];
     int valores[100];
   public:
-    Paleta(){
-      
+    Paleta(){ // inicializador vazio
+      quantidade = 0;
     }
-    
+    // ----------------
+    Paleta(int newQtd, std::initializer_list<Cor> cs, std::initializer_list<int> vs) { // inicializador pronto
+      quantidade = newQtd;
+      
+      int i = 0;
+      for (const auto& c : cs) cores[i++] = c;
+      
+      i = 0;
+      for (const auto& v : vs) valores[i++] = v;
+    }
+    // ----------------
+    void adicionar(Cor newCor, int newValor){ // adicionar uma cor no final
+      cores[quantidade] = newCor;
+      valores[quantidade] = newValor;
+      quantidade++;
+    }
+    // ----------------
+    Cor consultarCor(int thisValor){
+      for(int i = 0; i < quantidade; i++){
+        if(thisValor < valores[i]) return cores[i];
+      }
+      return {0,0,0};
+    }
+    // ----------------
+    std::string printar(){ // funçao de debug
+      std::string toOut = "Quantidade de cores: " + std::to_string(quantidade) + "\nCores:\n";
+      for(int i = 0; i < quantidade; i++){
+        toOut += "{" + std::to_string(cores[i].r) + " " +
+                  std::to_string(cores[i].g) + " " +
+                  std::to_string(cores[i].b) + "}\n";
+      }
+
+      toOut += "Valores:\n";
+      for(int i = 0; i < quantidade; i++){
+        toOut += std::to_string(valores[i]) + "\n";
+      }
+
+      return toOut;
+    }
+    // ----------------
+    void writePaleta(std::string ){
+
+    }
+    // ----------------
+    void readPaleta(std::string ){
+
+    }
+
   };
   
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <fstream>
+#include <iostream>
 #include "classes.hpp"
 
 int main(){
 
+  stc::Paleta thispaleta( 2, { {255,0,0},{0,255,0} }, { 100,200 } );
 
+  thispaleta.adicionar({0,0,255}, 205);
+
+  std::cout << thispaleta.atribuir(105).r << '\n';
+
+  std::cout << thispaleta.printar();
 
   return 0;
 }
