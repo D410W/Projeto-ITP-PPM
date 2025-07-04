3eb6c29ced6c3418149d704a0d8dba269071eb58
D410W <thalesjdc61@gmail.com>
2025-07-04 15:10:54 -0300

    'imagep3.hpp' adicionado ao repo, com inicializadores, destrutor e funçao de export. main.cpp ainda sendo utilizada para fins de debug.

---
+++ imagep3.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+#include "cor.hpp"
+
+namespace stc {
+  class ImageP3 {
+  private:
+    int height, width;
+    Cor *pixels = nullptr;
+    unsigned char maxCol = 255;
+    
+  public:
+    ImageP3(int h, int w) : height(h), width(w), maxCol(255), pixels(new Cor[h*w]) {}
+    ImageP3(int h, int w, int maxvalue) : height(h), width(w), maxCol(maxvalue), pixels(new Cor[h*w]) {}
+
+    ~ImageP3(){
+      delete[] pixels;
+    }
+    // ------------------
+    void exportf(std::string fileName){
+      if(!( std::filesystem::exists("out") && std::filesystem::is_directory("out") )){ // guardar arquivos na pasta 'out'
+        std::filesystem::create_directory("out");
+      }
+      
+      std::ofstream outputFile("out/"+fileName+".ppm");
+
+      outputFile << "P3\n" << this->width << ' ' << this->height << "\n" + std::to_string(this->maxCol) + "\n";
+      for(int i = 0; i < this->height; i++){
+        for(int j = 0; j < this->width; j++){
+          outputFile << (int)this->pixels[i*width + j].r << ' ' << (int)this->pixels[i*width + j].g << ' ' << (int)this->pixels[i*width + j].b;
+
+          if(j == this->width-1) outputFile << '\n';
+          else outputFile << ' ';
+        }
+      }
+      
+      outputFile.close();
+      return;
+    }
+    // ------------------
+    
+  };
+}
