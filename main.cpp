6b9a47046574ffebb3d56eeb40e4e0e22b048a2b
D410W <thalesjdc61@gmail.com>
2025-07-04 15:10:54 -0300

    'imagep3.hpp' adicionado ao repo, com inicializadores, destrutor e funçao de export. main.cpp ainda sendo utilizada para fins de debug.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <fstream>
 #include <iostream>
+
 #include "paleta.hpp"
+#include "imagep3.hpp"
 
 int main(){
 
-  stc::Paleta thispaleta( 2, { {250,0,0},{0,250,0} }, { 100,200 } );
+  // stc::Paleta thispaleta( 2, { {250,0,0},{0,250,0} }, { 100,200 } );
 
-  thispaleta.adicionar({0,0,255}, 205);
+  // thispaleta.adicionar({0,0,255}, 205);
 
-  std::cout << (int)thispaleta.getCor(99).r << '\n';
+  // std::cout << (int)thispaleta.getCor(99).r << '\n';
 
-  thispaleta.read("pal.txt");
+  // thispaleta.read("pal.txt");
 
-  std::cout << thispaleta.printar();
+  // std::cout << thispaleta.printar();
+
+  stc::ImageP3 *imagem = new stc::ImageP3(30,30);
+
+  (*imagem).exportf("teste");
 
   return 0;
 }
