02dc963ace798d65fd5a3bfddbcc195722148068
D410W <thalesjdc61@gmail.com>
2025-07-05 23:26:43 -0300

    adiçao do metodo de geraçao de noise 'Diamond-Square' na classe 'ImageP2'. esse metodo ainda devera passar por uma revisao, principalmente para melhor organizaçao da logica.
    ajustes nos construtores e destrutor da 'ImageP2', visando melhor alocaçao dinamica dos dados.
    classe 'ImageP3' devera passar por um ajuste semelhante.

--- includes/imagep2.hpp
+++ includes/imagep2.hpp
@@ -2,6 +2,10 @@
 
 #include <string>
 #include <filesystem>
+#include <cmath>
+#include <algorithm>
+#include <random>
+#include <vector>
 
 #include "cor.hpp"
 
@@ -12,12 +16,51 @@ namespace stc {
     unsigned char *pixels = nullptr;
     unsigned char maxValue = 255;
     
+    void deepCopy(const ImageP2& other) { // helper function for deep copying
+        height = other.height;
+        width = other.width;
+        maxValue = other.maxValue;
+        if (other.pixels) { // other.pixels != nullptr
+            pixels = new unsigned char[width * height];
+            std::copy(other.pixels, other.pixels + (width * height), pixels);
+        } else { // other.pixels == nullptr
+            pixels = nullptr;
+        }
+    }
   public:
-    ImageP2(int w, int h) : height(h), width(w), maxValue(255), pixels(new unsigned char[w*h]) {} // largura (x) e altura (y)
-    ImageP2(int w, int h, int newMax) : height(h), width(w), maxValue(newMax), pixels(new unsigned char[w*h]) {}
+    // initializer methods
+    ImageP2(int w, int h) : height(h), width(w), maxValue(255) { // inicializador apenas com largura e altura
+       if (w <= 0 || h <= 0) {
+        throw std::invalid_argument("Largura e altura devem ser positivas.");
+      }
+      this->pixels = new unsigned char[w * h];
+    }
+    ImageP2(int w, int h, int newMax) : height(h), width(w), maxValue(newMax) { // init. com largura, altura e valor max.
+      if (w <= 0 || h <= 0) {
+        throw std::invalid_argument("Largura e altura devem ser positivas.");
+      }
+      this->pixels = new unsigned char[w * h];
+    }
+    // copy methods
+    ImageP2(const ImageP2& other) : pixels(nullptr) { // copy initializer ()
+        deepCopy(other);
+    }
+    ImageP2& operator=(const ImageP2& other){ // copy assignment=
+        if(this == &other){
+            return *this;
+        }
 
+        delete[] pixels;
+        pixels = nullptr;
+
+        deepCopy(other);
+
+        return *this;
+    }
+    // destructor
     ~ImageP2(){
-      delete[] pixels;
+      delete[] this->pixels;
+      this->pixels = nullptr;
     }
     // ------------------
     int getWidth(){ return this->width; }
@@ -58,6 +101,9 @@ namespace stc {
       int value, w, h, mv;
       inputFile >> w >> h >> mv;
       
+      delete[] this->pixels;
+      this->pixels = new unsigned char[w*h];
+      
       this->width = w;
       this->height = h;
       this->maxValue = mv;
@@ -74,13 +120,117 @@ namespace stc {
     unsigned char getValue(int x, int y){
       return this->pixels[y * this->width + x];
     }
-    // ------------------
     void setValue(int x, int y, unsigned char newValue){
       this->pixels[y * this->width + x] = newValue;
     }
     // ------------------
-    void applyDiamSquare(int n, double rough, double weight){
-    
+    void addDiamSquare(int n, double rough, double weight){
+      if(n > 13 || n < 0){
+        throw std::invalid_argument("Valor de n \'perigoso\' na funçao Diamond-Square.");
+      }
+      int patternLength = (1 << n) + 1;
+      
+      std::vector<std::vector<unsigned char>> values(patternLength, std::vector<unsigned char>(patternLength));
+      
+      for(int i = 0; i < patternLength; i++){
+        for(int j = 0; j < patternLength; j++){
+          values[i][j] = 0;
+        }
+      }
+      
+      values[0][0] = std::rand()%256; // corners are set random.
+      values[0][patternLength-1] = std::rand()%256;
+      values[patternLength-1][0] = std::rand()%256;
+      values[patternLength-1][patternLength-1] = std::rand()%256;
+      
+      int currx, curry, step;
+      int currSideLen, halfSide; // represents 2^ni and 2^(ni-1)
+      for(int ni = 1; ni < n+1; ni++){
+        currx = 0, curry = 0;
+        step = (1 << (n-ni)); // distance from one point to another at the ni-th step.
+        currSideLen = (1 << ni); // represents 2^ni
+        halfSide = (1 << (ni - 1)); // represents 2^(ni-1)
+        // diamond
+        for(int i = 0; i < halfSide; i++){
+          for(int j = 0; j < halfSide; j++){
+            int toSet = 0;
+            
+            toSet += values[2*i*step][2*j*step]; // all 4 corners
+            toSet += values[2*i*step][2*(j+1)*step];
+            toSet += values[2*(i+1)*step][2*j*step];
+            toSet += values[2*(i+1)*step][2*(j+1)*step];
+            toSet /= 4;
+            toSet += ( std::rand()%33 - 16 )*rough;
+            
+            values[step + 2*i*step][step + 2*j*step] = std::clamp(toSet, 0, 255);
+          }
+        }
+        // square
+        for(int i = 0; i < currSideLen + 1; i++){
+          if(i%2 == 0){ // first collumn
+            for(int j = 0; j < halfSide; j++){
+              int toSet = 0;
+              int sums = 0;
+              
+              if(i > 0){
+                toSet += values[(i-1)*step][step + 2*j*step];
+                sums++;
+              }
+              if(i < currSideLen){
+                toSet += values[(i+1)*step][step + 2*j*step];
+                sums++;
+              }
+              if(j > 0){
+                toSet += values[i*step][2*j*step - step];
+                sums++;
+              }
+              if(j < halfSide - 1){
+                toSet += values[i*step][2*step + 2*j*step];
+                sums++;
+              }
+              toSet /= sums;
+              toSet += ( std::rand()%33 - 16 )*rough;
+              
+              values[i*step][step + 2*j*step] = std::clamp(toSet, 0, 255);
+            }
+          } else { // second collumn
+            for(int j = 0; j < halfSide + 1; j++){
+              int toSet = 0;
+              int sums = 0;
+            
+              if(i > 0){
+                toSet += values[(i-1)*step][2*j*step];
+                sums++;
+              }
+              if(i < currSideLen){
+                toSet += values[(i+1)*step][2*j*step];
+                sums++;
+              }
+              if(j > 0){
+                toSet += values[i*step][2*j*step - step];
+                sums++;
+              }
+              if(j < halfSide - 1){
+                toSet += values[i*step][step + 2*j*step];
+                sums++;
+              }
+              toSet /= sums;
+              toSet += ( std::rand()%33 - 16 )*rough;
+            
+              values[i*step][2*j*step] = std::clamp(toSet, 0, 255);
+            }
+          }
+        }
+        
+      }
+      // return values
+      for(int i = 0; i < std::min(patternLength, this->width); i++){
+        for(int j = 0; j < std::min(patternLength, this->height); j++){
+          this->pixels[j*(this->width) + i] = values[i][j]*weight;
+        }
+      }
+      
+      return;
     }
     // ------------------
   };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+
+#include <random>
+#include <ctime>
 
 #include "includes/header.hpp"
 
 int main(){
-
+  std::srand(std::time(0));
   
+  int n;
+  double rough;
+  std::cin >> n >> rough;
+
+  stc::ImageP2 mapa((1 << n) + 1, (1 << n)+ 1);
+  
+  mapa.addDiamSquare(n, rough, 1);
+  
+  mapa.write("diamond.ppm");
 
   return 0;
 }
