cd1dc1b0b06b874fdf34359714526652bacff5fd
D410W <thalesjdc61@gmail.com>
2025-07-10 15:44:20 -0300

    Configuraçao do CMake no projeto, e organizaçao correspondente dos diretorios.

---
+++ build/CMakeFiles/my_executable.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_executable.dir/src/includes/ImageP2/imagep2.cpp.o"
+  "CMakeFiles/my_executable.dir/src/includes/ImageP2/imagep2.cpp.o.d"
+  "CMakeFiles/my_executable.dir/src/includes/ImageP3/imagep3.cpp.o"
+  "CMakeFiles/my_executable.dir/src/includes/ImageP3/imagep3.cpp.o.d"
+  "CMakeFiles/my_executable.dir/src/includes/Paleta/paleta.cpp.o"
+  "CMakeFiles/my_executable.dir/src/includes/Paleta/paleta.cpp.o.d"
+  "CMakeFiles/my_executable.dir/src/main.cpp.o"
+  "CMakeFiles/my_executable.dir/src/main.cpp.o.d"
+  "my_executable"
+  "my_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
